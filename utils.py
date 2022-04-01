23a627ab1d31e66f8d8fdae28e59a1b3e83cee0d
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2022-04-01 12:53:36 +0530

    init

---
+++ utils.py
@@ -0,0 +1,103 @@
+import math
+
+def encodeMessage(image, msg, fileName):
+    msg = "." + msg
+    width, height = image.size
+    if (width * height) < len(msg):
+        raise ValueError
+    pixels = image.load()
+    for i in range(0, len(msg)):
+        encodedChar = str(ord(msg[i]))
+        row = math.ceil(i/float(width))
+        column = i%width
+        pixel = pixels[row-1, column-1]
+        newPixel = list(pixel)
+        newPixel[0] = str(newPixel[0])
+        newPixel[1] = str(newPixel[1])
+        newPixel[2] = str(newPixel[2])
+
+        if ord(msg[i]) >= 100:
+            r = list(newPixel[0])
+            r[-1] = encodedChar[0]
+            newPixel[0] = ''.join(r)
+
+            r = list(newPixel[1])
+            r[-1] = encodedChar[1]
+            newPixel[1] = ''.join(r)
+
+            r = list(newPixel[2])
+            r[-1] = encodedChar[2]
+            newPixel[2] = ''.join(r)
+
+        elif ord(msg[i]) >= 10:
+            r = list(newPixel[0])
+            r[-1] = '0'
+            newPixel[0] = ''.join(r)
+
+            r = list(newPixel[1])
+            r[-1] = encodedChar[0]
+            newPixel[1] = ''.join(r)
+
+            r = list(newPixel[2])
+            r[-1] = encodedChar[1]
+            newPixel[2] = ''.join(r)
+
+        else:
+            r = list(newPixel[0])
+            r[-1] = '0'
+            newPixel[0] = ''.join(r)
+
+            r = list(newPixel[1])
+            r[-1] = '0'
+            newPixel[1] = ''.join(r)
+
+            r = list(newPixel[2])
+            r[-1] = encodedChar[0]
+            newPixel[2] = ''.join(r)
+
+
+        newPixel[0] = int(newPixel[0])
+        newPixel[1] = int(newPixel[1])
+        newPixel[2] = int(newPixel[2])
+        pixels[row-1, column-1] = tuple(newPixel)
+    row = math.ceil(len(msg)/float(width))
+    column = len(msg)%width
+    pixel = pixels[row, column]
+    newPixel = list(pixel)
+    newPixel[0] = str(newPixel[0])
+    newPixel[1] = str(newPixel[1])
+    newPixel[2] = str(newPixel[2])
+
+    r = list(newPixel[0])
+    r[-1] = '1'
+    newPixel[0] = ''.join(r)
+    
+    r = list(newPixel[1])
+    r[-1] = '2'
+    newPixel[1] = ''.join(r)
+
+    r = list(newPixel[2])
+    r[-1] = '7'
+    newPixel[2] = ''.join(r)
+
+
+
+    newPixel[0] = int(newPixel[0])
+    newPixel[1] = int(newPixel[1])
+    newPixel[2] = int(newPixel[2])
+    pixels[row-1, column-1] = tuple(newPixel)
+    image.save("./static/" + fileName)
+
+
+def decodeMessage(image):
+    pixels = image.load()
+    encodedText = ""
+    for w in range(0, image.size[0]):
+        for h in range(0, image.size[1]):
+            pixel = pixels[w,h]
+            encodedChar = int(str(pixel[0])[-1]) * 100 + int(str(pixel[1])[-1]) * 10 + int(str(pixel[2])[-1])
+            if(encodedChar == 127):
+                return encodedText
+            else:
+                encodedText+=chr(encodedChar)
+    return encodedText
